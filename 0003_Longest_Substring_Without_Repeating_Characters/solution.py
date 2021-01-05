1b621406c3d897824f2ad6a59ba06b2bec33fc70
oza <oza@acecamel.com>
2021-01-05 16:59:05 +0800

    solution 2 for #3

--- 0003_Longest_Substring_Without_Repeating_Characters/solution.py
+++ 0003_Longest_Substring_Without_Repeating_Characters/solution.py
@@ -1,21 +1,38 @@
 class Solution:
-    # 第一次解法，双重循环嵌套
+    # Solution 1
+    # def lengthOfLongestSubstring(self, s: str) -> int:
+    #     max_len = 0
+    #     for i in range(len(s)):
+    #         char_set = set()
+    #         length = 0
+    #         for j in range(i, len(s)):
+    #             if s[j] in char_set:
+    #                 break
+    #             length += 1
+    #             char_set.add(s[j])
+    #         if length > max_len:
+    #             max_len = length
+    #     return max_len
+
+    # Solution 2
     def lengthOfLongestSubstring(self, s: str) -> int:
-        max_len = 0
-        for i in range(len(s)):
-            char_set = set()
-            length = 0
-            for j in range(i, len(s)):
-                if s[j] in char_set:
-                    break
-                length += 1
-                char_set.add(s[j])
-            if length > max_len:
-                max_len = length
+        r_cur, max_len = -1, 0
+        char_set = set()
+        
+        for l_cur in range(len(s)):
+            if l_cur != 0:
+                char_set.remove(s[l_cur - 1])   # 每次循环开始, 去除 set 里面的上一起始位置的元素
+            
+            while r_cur + 1 < len(s) and s[r_cur + 1] not in char_set:
+                # 当右指针下一位置所在的元素没出现过时，右指针右移到此位置
+                char_set.add(s[r_cur + 1])
+                r_cur += 1
+            # 当开始出现重复字符时，更新最大长度的值
+            max_len = max(max_len, r_cur - l_cur + 1)
+        
         return max_len
 
-    # def lengthOfLongestSubstring(self, s: str) -> int:
-    #     pass
+
 
 
 if __name__ == "__main__":
